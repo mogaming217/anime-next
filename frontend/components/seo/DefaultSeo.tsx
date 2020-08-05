56aca6886d28601b8505af5d9817c8e9615fe825
mogaming <seiya.orz@gmail.com>
2020-08-05 23:20:35 +0900

    fix desc

--- frontend/components/seo/DefaultSeo.tsx
+++ frontend/components/seo/DefaultSeo.tsx
@@ -5,7 +5,7 @@ import { Const } from 'lib/const'
 export const DefaultSeo = () => <DS
   openGraph={{
     type: 'website',
-    title: `${Const.SERVICE_NAME}｜アニメの続きを楽しめる原作情報が探せるサービス`,
+    title: `${Const.SERVICE_NAME}｜アニメの続きの原作情報が探せるサービス`,
     description: 'おもしろかったアニメの続きは原作の何巻から読めるのかがすぐ見つかる！原作のマンガやライトノベル、小説をすぐ購入できるサービスです。',
     url: publicEnv.host,
     images: [{
