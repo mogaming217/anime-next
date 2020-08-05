7c8e12fab71ba41712bdc68b4a09b93ad3cdeb28
mogaming <seiya.orz@gmail.com>
2020-08-05 23:12:13 +0900

    add about page

--- frontend/lib/const.ts
+++ frontend/lib/const.ts
@@ -1,6 +1,6 @@
 import { publicEnv } from "env";
 
 export const Const = {
-  SERVICE_NAME: 'アニオリ',
+  SERVICE_NAME: 'アニメノツヅキ',
   DEFAULT_OG_IMAGE: `${publicEnv.host}/assets/ogimage/default.png`
 }
