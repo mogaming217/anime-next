2a3e54b2f0d75408557de7ab2abc0f2cfb4f8fa9
mogaming <seiya.orz@gmail.com>
2021-01-21 11:35:42 +0900

    FIX: meta data

--- frontend/components/seo/DefaultSeo.tsx
+++ frontend/components/seo/DefaultSeo.tsx
@@ -2,23 +2,28 @@ import { DefaultSeo as DS } from 'next-seo'
 import { publicEnv } from 'env'
 import { Const } from 'lib/const'
 
-export const DefaultSeo = () => (
-  <DS
-    openGraph={{
-      type: 'website',
-      title: `${Const.SERVICE_NAME}｜アニメの続きの原作情報が探せるサービス`,
-      description:
-        'おもしろかったアニメの続きは原作の何巻から読めるのかがすぐ見つかる！原作のマンガやライトノベル、小説をすぐ購入できるサービスです。',
-      url: publicEnv.host,
-      images: [
-        {
-          url: Const.DEFAULT_OG_IMAGE,
-        },
-      ],
-      locale: 'ja_JP',
-    }}
-    twitter={{
-      cardType: 'summary_large_image',
-    }}
-  />
-)
+export const DefaultSeo = () => {
+  const title = `${Const.SERVICE_NAME}｜アニメの続きの原作情報が探せるサービス`
+  const description =
+    'おもしろかったアニメの続きは原作の何巻から読めるのかがすぐ見つかる！原作のマンガやライトノベル、小説をすぐ購入できるサービスです。'
+  return (
+    <DS
+      title={title}
+      openGraph={{
+        type: 'website',
+        title,
+        description,
+        url: publicEnv.host,
+        images: [
+          {
+            url: Const.DEFAULT_OG_IMAGE,
+          },
+        ],
+        locale: 'ja_JP',
+      }}
+      twitter={{
+        cardType: 'summary_large_image',
+      }}
+    />
+  )
+}
